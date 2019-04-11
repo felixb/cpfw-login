463a86e90464346427fd6a066451af75ff47a9e5
Martin Knudsen <martin.knudsen@otto.de>
2019-04-11 17:19:52 +0200

    work around breaking change in cookie struct from go 1.11 to 1.12

---
+++ cookie_old.go
@@ -0,0 +1,16 @@
+// +build !go1.12
+
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func loginTypeCookie(host string, time time.Time) *http.Cookie {
+	return &http.Cookie{"cpnacportal_login_type", "password", "/", host, time, "", 0, true, false, "password", []string{"password"}}
+}
+
+func userCookie(host string, time time.Time, user string) *http.Cookie {
+	return &http.Cookie{"cpnacportal_username", user, "/", host, time, "", 0, true, false, "password", []string{user}}
+}
